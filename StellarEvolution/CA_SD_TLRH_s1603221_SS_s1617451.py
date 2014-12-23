27275dab4ecf1e408941bc95052802f24cf2ac3e
Timo Halbesma <timohalbesma@gmail.com>
2014-12-23 18:22:27 +0100

    assignment 2d code

--- StellarEvolution/CA_SD_TLRH_s1603221_SS_s1617451.py
+++ StellarEvolution/CA_SD_TLRH_s1603221_SS_s1617451.py
@@ -23,9 +23,10 @@ from amuse.community.mesa.interface import MESA
 
 from amuse.lab import Particles
 
-from amuse.lab import new_king_model, ph4
+from amuse.lab import new_plummer_sphere, ph4
 from amuse.units import units, nbody_system
 from amuse.lab import set_printing_strategy
+from amuse.community.bhtree.interface import BHTree
 
 from amuse.rfi.core import is_mpd_running
 from amuse.ic.salpeter import new_salpeter_mass_distribution
@@ -247,25 +248,128 @@ def assignment_2b():
     #                          1000 | units.Myr, 0.17)
 
 
+def stellar_remnant_state(star):
+    return 10 <= star.stellar_type.value_in(units.stellar_type) and \
+        star.stellar_type.value_in(units.stellar_type) < 16
+
+def stellar_main_sequence_state(star):
+    return star.stellar_type.value_in(units.stellar_type) in (0,1,7)
+
+def stellar_giant_state(star):
+    return star.stellar_type.value_in(units.stellar_type) in (2,3,4,5,6,8,9)
+
 def assignment_2d():
-    N = 1024
-    Rvir = 10 | units.parsec
+    current_cluster_mass = 400 | units.MSun
+    initial_mass_fraction = 0.84
+    desired_initial_mass = current_cluster_mass / initial_mass_fraction
+
+    masses = new_salpeter_mass_distribution(100000)
+    mean_salpeter_mass = masses.mean()
+    print "mean salpeter mass", mean_salpeter_mass
+    N = int(desired_initial_mass / mean_salpeter_mass)
+    print "N", N
+
+
+    Rvir = 10 | units.lightyear
+    z = 0.17
     masses = new_salpeter_mass_distribution(N)
     converter = nbody_system.nbody_to_si(masses.sum(), Rvir)
-    bodies = new_king_model(N, W0, convert_nbody=converter)
+    G_SI = converter.to_si(nbody_system.G)
+    bodies = new_plummer_sphere(N, convert_nbody=converter)
     bodies.mass = masses
-    bodies.scale_to_standard(converter)
+    bodies.metalicity = z
 
-    # star the gravity solver
-    gravity = ph4(converter)
+    # start the gravity solver
+    gravity = BHTree(converter)
+    gravity.initialize_code()
+    gravity.parameters.timestep = 0.1 | units.Myr
+
+    # start the stellar evolution solver
+    stellar = SSE()
+    stars = stellar.particles.add_particles(bodies)
+    from_stellar_evolution_to_model \
+        = stellar.particles.new_channel_to(bodies)
+    from_stellar_evolution_to_model.copy_attributes(["mass"])
+
+    bodies.scale_to_standard(converter)
     gravity.particles.add_particles(bodies)
 
-    # star the stellar evolution solver
-    stellar = MESA()
-    stellar.particles.add_particles(bodies)
+    from_model_to_gravity = bodies.new_channel_to(gravity.particles)
+    from_gravity_to_model = gravity.particles.new_channel_to(bodies)
+    gravity.commit_particles()
+
+    end_time = 1000 | units.Myr
+    current_time = 0 | units.Myr
+    cluster = "Hyades"
+    bound_stars_counts = []
+    main_sequence_stars_counts = []
+    giant_stars_counts = []
+    remnant_stars_counts = []
+    max_radii = [] | units.parsec
+    virial_radii = [] | units.parsec
+    times = [] | units.Myr
+    while current_time < end_time:
+        name_of_the_figure = "isochrone_with_grav_"+str(int(current_time.value_in(units.Myr)))+".png"
+
+        gravity.evolve_model(current_time)
+        stellar.evolve_model(current_time)
+
+
+        from_gravity_to_model.copy()
+        from_stellar_evolution_to_model.copy_attributes(["mass", "radius"])
+        from_model_to_gravity.copy_attributes(["mass"])
+
+
+        remnant_count = 0
+        main_sequence_count = 0
+        giant_count = 0
+        for star in stars:
+            if stellar_remnant_state(star):
+                remnant_count += 1
+            if stellar_giant_state(star):
+                giant_count += 1
+            if stellar_main_sequence_state(star):
+                main_sequence_count += 1
+        max_radius = bodies.total_radius()
+        virial_radius = bodies.virial_radius()
+        bound_star_count = len(bodies.bound_subset(unit_converter=converter, G=G_SI))
+        print "bound stars:", bound_star_count
+        print "main sequence stars:", main_sequence_count
+        print "giant stars:", giant_count
+        print "remnant stars:", remnant_count
+        print "cluster radius(max from centre):", max_radius
+        print "virial radius:", virial_radius
+        print current_time
+
+        times.append(current_time)
+        remnant_stars_counts.append(remnant_count)
+        giant_stars_counts.append(giant_count)
+        main_sequence_stars_counts.append(main_sequence_count)
+        max_radii.append(max_radius)
+        virial_radii.append(virial_radius)
+        bound_stars_counts.append(bound_star_count)
+
+
+        temperatures = stars.temperature
+        luminosities = stars.luminosity
+
+        plot_HR_diagram(temperatures, luminosities,
+                        cluster+"/",
+                        name_of_the_figure, current_time)
+        current_time += 10 | units.Myr
+    data = {}
+    data["bound_stars_at_time"] = bound_stars_counts
+    data["remnant_stars_at_time"] = remnant_stars_counts
+    data["giant_stars_at_time"] = giant_stars_counts
+    data["main_sequence_stars_at_time"] = main_sequence_stars_counts
+    data["max_radius_at_time"] = max_radii
+    data["virial_radii"] = virial_radii
+    data["times"] = times
+    pickle.dump(data, open(cluster+"/assignment2d.dat", "wb"))
 
 
 if __name__ == '__main__':
     assert is_mpd_running()
     #assignment_2a()
-    assignment_2b()
+    #assignment_2b()
+    assignment_2d()
