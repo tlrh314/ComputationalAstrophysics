7016b71c3e4865a3f3931ba3a9211809ef9f1ded
tlrh314 <timohalbesma@gmail.com>
2015-01-04 08:10:44 +0100

    Added title and legend

--- HydroDynamics/plot_endstates.py
+++ HydroDynamics/plot_endstates.py
@@ -1,32 +1,35 @@
 import os
+
 import matplotlib
 matplotlib.use("Agg")
+from matplotlib import pyplot
 
-from matplotlib import pyplot, animation
 from amuse import plot as aplot
-
-from amuse.support.console import set_printing_strategy
 from amuse.io import read_set_from_file
-from amuse.units import units, optparse
+from amuse.units import units
 
 if __name__ == "__main__":
-    for i in xrange(20, 301, 20):
-        impact_parameter = i|units.parsec
-        for j in xrange(25, 201, 25):
-            v_infinity = (j/100.0)|units.kms
+    # for i in xrange(5, 21, 5):
+    # for i in xrange(20, 301, 20):
+    for i in [20]:
+        impact_parameter = i | units.parsec
+        # for j in xrange(5, 26, 5):
+        # for j in xrange(25, 201, 25):
+        for j in [15]:
+            v_infinity = (j/100.0) | units.kms
 
             data_file = "simulation_data/v_"+str(j)+"_d_"+str(i)+".dat"
             out_file = "simulation_end_states/v_"+str(j)+"_d_"+str(i)+".png"
-            
-            if os.path.isfile(out_file):
-                print "file " + out_file + " already exists, skip plotting"
-                continue
+
+            # if os.path.isfile(out_file):
+            #    print "file " + out_file + " already exists, skip plotting"
+            #    continue
             print "ouputing " + out_file
-            cluster, gascloud = read_set_from_file(data_file, 'amuse', 
-                    names=("cluster", "gas"))
+            cluster, gascloud = read_set_from_file(data_file, 'amuse',
+                                                   names=("cluster", "gas"))
             lim = abs(next(cluster.history).position).max()
-            
-            fig = pyplot.figure(figsize=[10,10])
+
+            fig = pyplot.figure(figsize=[10, 10])
 
             artists = []
             cl = None
@@ -34,19 +37,24 @@ if __name__ == "__main__":
             for c, g in zip(cluster.history, gascloud.history):
                 cl = c
                 gas = g
-            points = aplot.scatter(cl.x, cl.y, c='black')
-            gas_points = aplot.scatter(gas.x, gas.y, s=100, c='b', 
-                    edgecolors='none', alpha=0.3, rasterized=True)
+            points = aplot.scatter(cl.x, cl.y, c='black',
+                                   label='Stellar Cluster')
+            gas_points = aplot.scatter(gas.x, gas.y, s=100, c='b',
+                                       edgecolors='none', alpha=0.3,
+                                       rasterized=True,
+                                       label='Giant Molecular Cloud')
 
-            time_label = "t = {:.2f} Myr".format(
-                    cl.get_timestamp().value_in(units.Myr))
+            time_label = r'$t_{\rm end}$ ' + "= {:.2f} Myr".format(cl.get_timestamp().
+                                                 value_in(units.Myr))
             text = aplot.text(-4./5.*lim, 4./5.*lim, time_label)
             aplot.xlabel("X")
             aplot.ylabel("Y")
             pyplot.axis('equal')
             aplot.xlim(-lim, lim)
             aplot.ylim(-lim, lim)
+            pyplot.legend()
+            pyplot.title(r'Cluster orbitting GMC with $d$={0}, $v_\infty$={1}'
+                         .format(impact_parameter, v_infinity) +
+                         r', at $t_{\rm end}$')
             pyplot.savefig(out_file)
             pyplot.close()
-                        
-
